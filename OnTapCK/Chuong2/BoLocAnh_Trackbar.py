fc7b43c7d456eb3a2b6ffe1108ce2bb4fea88773
vannducs <nguyenvanducqlfptshop@gmail.com>
2026-06-14 07:46:39 +0700

    bl

--- OnTapCK/Chuong2/BoLocAnh_Trackbar.py
+++ OnTapCK/Chuong2/BoLocAnh_Trackbar.py
@@ -1,24 +1,27 @@
 #Tạo từng trackbar cho từng bộ lọc và xem sự thay đổi của ảnh
 import cv2
-img=cv2.imread(r"C:\Users\DELL\Downloads\he.jpg")
-cv2.namedWindow("Trackbar")
-def nothing(x): pass
-cv2.createTrackbar("Blur","Trackbar",0,10,nothing)
-cv2.createTrackbar("medianBlur","Trackbar",0,10,nothing)
-cv2.createTrackbar("GaussianBlur","Trackbar",0,10,nothing)
-cv2.createTrackbar("bilateralFilter","Trackbar",0,10,nothing)
+img = cv2.imread(r"C:\Users\DELL\Downloads\he.jpg")
+cv2.namedWindow("Anh")
+def nothing(x):pass
+cv2.createTrackbar("Trungbinh","Anh",1,5,nothing)
+cv2.createTrackbar("Trungvi","Anh",1,5,nothing)
+cv2.createTrackbar("Gauss","Anh",1,5,nothing)
+cv2.createTrackbar("Songphuong","Anh",1,10,nothing)
 while True:
-    bl=cv2.getTrackbarPos("Blur","Trackbar")
-    me=cv2.getTrackbarPos("medianBlur","Trackbar")*2+1
-    ga=cv2.getTrackbarPos("GaussianBlur","Trackbar")*2+1
-    bi=cv2.getTrackbarPos("bilateralFilter","Trackbar")
-    if bl==0: bl=1 
-    if bi==0: bi=1
-    kq=cv2.blur(img,(bl,bl))
-    kq=cv2.medianBlur(kq,me)
-    kq=cv2.GaussianBlur(kq,(ga,ga),0)
-    kq=cv2.bilateralFilter(kq,bi,75,75)
-    cv2.imshow("Trackbar",kq)
-    if cv2.waitKey(1)==ord("q"):break
+    tb = cv2.getTrackbarPos("Trungbinh","Anh")
+    tv = cv2.getTrackbarPos("Trungvi","Anh")*2+1
+    gauss = cv2.getTrackbarPos("Gauss","Anh")*2+1
+    sp = cv2.getTrackbarPos("Songphuong","Anh")
+    if tb==0: tb=1
+    if sp==0: sp=1
+    imgtb = cv2.blur(img, (tb,tb))
+    imgtv = cv2.medianBlur(imgtb, tv)
+    imgg = cv2.GaussianBlur(imgtv, (gauss,gauss),0)
+    imgsp = cv2.bilateralFilter(imgg, sp, 75,75)
+    cv2.imshow("Anh",imgsp)
+    if cv2.waitKey(1)==ord("q"):
+        break
 
 cv2.destroyAllWindows()
+
+
